42e47b6e7aa15aa2c5c2879c901c8c7ae1142ad3
jack tan <62325379+jacktan130802@users.noreply.github.com>
2021-03-18 21:31:19 +0800

    TEST

--- app/src/main/java/com/sp/test/Notes5.java
+++ app/src/main/java/com/sp/test/Notes5.java
@@ -1,14 +1,25 @@
 package com.sp.test;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class Notes5 extends AppCompatActivity {
+    private RecyclerView recyclerView ;
+    private NotesAdapter5 adapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes5);
+        recyclerView = findViewById(R.id.recycler5);
+        layoutManager = new LinearLayoutManager(this);
+        adapter = new NotesAdapter5();
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
     }
 }
